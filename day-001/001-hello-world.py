b400fae0ce011653070f453fe2cf3b44e5e8e67b
Neil Allwood <neilallwood@icloud.com>
2021-11-02 15:47:32 +0000

    First commit

---
+++ day-001/001-hello-world.py
@@ -0,0 +1,12 @@
+# output "Hello World!!" to the console
+print("Hello World!!")
+
+# if you need to use quotations within a print output of a string, there are two options you can do:
+
+# 1. use different quote marks as python will recognise the difference:
+print("An example of 'a string'")
+# or
+print('An example of "a string"')
+
+# 2. you can use the \ as an escape charecter to get around python getting muddled up:
+print("An example of \"a string\"")
