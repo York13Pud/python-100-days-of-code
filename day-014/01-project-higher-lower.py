f660d4549d0b5d87d3f3867592d034492e0809a0
Neil Allwood <neilallwood@icloud.com>
2021-12-24 13:36:53 +0000

    Added a template for python called template.py

---
+++ day-014/01-project-higher-lower.py
@@ -0,0 +1,9 @@
+# Import the data for the game and the artwork:
+from art import logo, vs
+from game_data import data
+import random
+
+
+items_in_data = len(data)
+print(items_in_data)
+#print(random.randint(range(1,items_in_data)))
